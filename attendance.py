e7d6a99ea7f3046a449363e4282f7cd673aeb28b
William H. Grover <wgrover@gmail.com>
2023-12-22 15:27:59 -0800

    attendance service for systemctl

--- attendance.py
+++ attendance.py
@@ -1,8 +1,12 @@
 import keyboard  # pip install keyboard
 import time
 
+f = open("/home/wgrover/attendance/out.txt", "a")
+
 def key_press(key):
     print(key.name)
+    f.write(key.name)
+    f.flush()
     if key.name.isnumeric():
         print("NUMBERWANG")
 
@@ -12,4 +16,4 @@ keyboard.on_press(key_press)
 while True:
     time.sleep(1)
 
-
+f.close()
